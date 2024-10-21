78d37232ef38a80eb614a094f4cf79e25c82f867
aynazmeymanat <aynazmeymanat@gmail.com>
2024-10-21 23:58:53 +0330

    write fields

--- src/Library.java
+++ src/Library.java
@@ -1,9 +1,8 @@
 import java.util.List;
 
-public class Library {
     public class Library {
         private String name;
         private List<Book> books;
         private List<Member> members;
 
-}
+    }
