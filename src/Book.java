18e1334ed60bae241732b1d3788f3f5438f79d84
aynazmeymanat <aynazmeymanat@gmail.com>
2024-10-22 00:35:48 +0330

    in Book class field and constructor were written

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,21 @@
 public class Book {
+        private String id;
+        private String title;
+        private String author;
+        private boolean isAvailable;
+        private Genre genre;
+        private int borrowCount;
 
+    public Book(String id, String title, String author, Genre genre) {
+        if (id.length() == 5) {
+            this.id = id;
+        } else {
+            System.out.println("invalid");
+            System.exit(0);
+        }
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+        this.borrowCount = 0;
+    }
 }
