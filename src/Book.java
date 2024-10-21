fbd66cb81924d0a31f5238f0df2c6058e8d280fd
aynazmeymanat <aynazmeymanat@gmail.com>
2024-10-22 00:43:09 +0330

    The methods and getter and setter  were written in Book class

--- src/Book.java
+++ src/Book.java
@@ -18,4 +18,66 @@ public class Book {
         this.isAvailable = true;
         this.borrowCount = 0;
     }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        if (id.length() != 5) {
+            System.out.println("");
+        }
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public int getBorrowCount() {
+        return borrowCount;
+    }
+
+    public void setBorrowCount(int borrowCount) {
+        this.borrowCount = borrowCount;
+    }
+
+    public void updateCount() {
+        this.borrowCount++;
+    }
+
+    public void capitalizeTitle() {
+        String[] words = title.split(" ");
+        StringBuilder capitalizedTitle = new StringBuilder();
+        for (String word : words) {
+            capitalizedTitle.append(Character.toUpperCase(word.charAt(0)))
+                    .append(word.substring(1).toLowerCase())
+                    .append(" ");
+        }
+        this.title = capitalizedTitle.toString().trim();
+    }
+
+    public void printBookInfo() {
+        System.out.println("ID: " + id);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Available: " + isAvailable);
+        System.out.println("Genre: " + genre);
+        System.out.println("Borrow Count: " + borrowCount);
+    }
 }
